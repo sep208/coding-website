cbbe18eef74ccc1d61227420bf16bc21beb2abda
Jayden Bishop <>
2021-04-20 00:10:43 +0000

    changed app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import flask 
+import flask
+import random 
 from flask import  Flask, redirect, url_for, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
@@ -10,5 +11,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    #DEBUG is SET to TRUE. CHANGE FOR PROD
-    app.run(port=5000,debug=True)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
